e66846d4ca0d6c3796fe0a2685b3c2d71fa3bc2d
nonchalamment <nonchalamment@gmail.com>
2023-04-27 11:16:05 -0400

    add favicon

---
+++ modules/calendar.js
@@ -0,0 +1,7 @@
+import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
+import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
+
+const calendar = new VanillaCalendar('#calendar');
+calendar.init();
