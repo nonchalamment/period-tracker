d2c8c833fb84200d1d501d6d13902f664683d203
nonchalamment <nonchalamment@gmail.com>
2023-04-27 23:08:18 -0400

    fix styling for forms, h1, h2

--- modules/calendar.js
+++
@@ -1,7 +0,0 @@
-import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
-import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
-
-const calendarEl = document.querySelector('#calendar');
-new VanillaCalendar(calendarEl);
